7c1da91974d83ac04b1a1601746fa607dbff8fa1
Trixesu <trixteresu@gmail.com>
2024-10-31 08:52:08 +0700

    Update eAgamaTipeEnum.java

--- eAgamaTipeEnum.java
+++ eAgamaTipeEnum.java
@@ -1,10 +1,10 @@
-package modulv;
+package belajar.module5;
 
 import java.util.Scanner;
 
 public class eAgamaTipeEnum {
     enum Agama {
-        HINDU, BUDDHA, YAHUDI, KATOLIK, KRISTEN, ISLAM
+        HINDU, BUDDHA, YAHUDI, KATOLIK, KRISTEN, ISLAM, ATHEIS
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -12,8 +12,8 @@ public class eAgamaTipeEnum {
         System.out.print("Agama \t\t: ");
         String inputAgama = input.nextLine().toUpperCase();
         
-        String kitabSuci = "";
-        String nabi = "";
+        String kitabSuci;
+        String nabi;
         
         try {
             Agama agama = Agama.valueOf(inputAgama);
@@ -22,35 +22,48 @@ public class eAgamaTipeEnum {
                 case HINDU:
                     kitabSuci = "Veda";
                     nabi = "-";
+                    System.out.println("Kitab Suci \t: "+kitabSuci);
+                    System.out.println("Nabi \t\t: "+nabi);
                     break;
                 case BUDDHA:
                     kitabSuci = "Tripitaka";
                     nabi = "Sidharta Gautama";
+                    System.out.println("Kitab Suci \t: "+kitabSuci);
+                    System.out.println("Nabi \t\t: "+nabi);
                     break;
                 case YAHUDI:
                     kitabSuci = "Taurat";
                     nabi = "Musa";
+                    System.out.println("Kitab Suci \t: "+kitabSuci);
+                    System.out.println("Nabi \t\t: "+nabi);
                     break;
                 case KATOLIK:
                     kitabSuci = "Injil";
                     nabi = "Yesus";
+                    System.out.println("Kitab Suci \t: "+kitabSuci);
+                    System.out.println("Nabi \t\t: "+nabi);
                     break;
                 case KRISTEN:
                     kitabSuci = "Injil";
                     nabi = "Yesus";
+                    System.out.println("Kitab Suci \t: "+kitabSuci);
+                    System.out.println("Nabi \t\t: "+nabi);
                     break;
                 case ISLAM:
                     kitabSuci = "Alquran";
                     nabi = "Muhammad";
+                    System.out.println("Kitab Suci \t: "+kitabSuci);
+                    System.out.println("Nabi \t\t: "+nabi);
+                    break;
+                case ATHEIS:
+                    System.out.println("Ngapain jirrr");
                     break;
             }
         } catch (IllegalArgumentException e){
                 System.out.println("Agama tidak valid");
                 input.close();
-                return;
+                    return;
         }
-        System.out.println("Kitab Suci \t: "+kitabSuci);
-        System.out.println("Nabi \t\t: "+nabi);
         input.close();
     }
 }
