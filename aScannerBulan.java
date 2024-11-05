b06c2bac69d5cc93948be218cabf1c4f35b3645c
Trixesu <trixteresu@gmail.com>
2024-11-05 19:36:01 +0700

    Update aScannerBulan.java

--- aScannerBulan.java
+++ aScannerBulan.java
@@ -10,32 +10,32 @@ public class aScannerBulan {
         int month = input.nextInt();
         String monthString;
         
-        switch (month) {
-            case 1:  monthString = "Januari";
+         switch (month) {
+            case 1:  monthString = "January";
                      break;
-            case 2:  monthString = "Februari";
+            case 2:  monthString = "February";
                      break;
-            case 3:  monthString = "Maret";
+            case 3:  monthString = "March";
                      break;
             case 4:  monthString = "April";
                      break;
-            case 5:  monthString = "Mei";
+            case 5:  monthString = "May";
                      break;
-            case 6:  monthString = "Juni";
+            case 6:  monthString = "June";
                      break;
-            case 7:  monthString = "Juli";
+            case 7:  monthString = "July";
                      break;
-            case 8:  monthString = "Agustus";
+            case 8:  monthString = "August";
                      break;
             case 9:  monthString = "September";
                      break;
-            case 10: monthString = "Oktober";
+            case 10: monthString = "October";
                      break;
             case 11: monthString = "November";
                      break;
-            case 12: monthString = "Desember";
+            case 12: monthString = "December";
                      break;
-            default: monthString = "Bulan invalid";
+            default: monthString = "Invalid month";
                      break;
         }
         System.out.println(monthString);
